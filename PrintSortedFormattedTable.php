ffec1feaaacfafea026703a48b1495584a39450e
Christiaan Nieuwlaat <krizzje@Gmail.com>
2016-05-02 07:32:04 +0200

    Create PrintSortedFormattedTable.php

---
+++ PrintSortedFormattedTable.php
@@ -0,0 +1,53 @@
+<?php
+
+class Person { 
+	public $initials;
+	public $firstname;
+	public $middlename;
+	public $lastname;
+	public $dateofbirth;
+	
+	
+	public function __construct($Initials = '', $FirstName = '', $MiddleName = '', $LastName = '', $dob) {
+		$this->initials = $Initials;
+		$this->firstname = $FirstName;
+		$this->middlename = $MiddleName;
+		$this->lastname = $LastName;
+		$this->dateofbirth = $dob;
+	}
+}
+
+
+$data = [];
+$data[] = new Person('p','Pieter-Argibald','','Puk', new DateTime('1989/02/01'));
+$data[] = new Person('P','Petra','','Puk', new DateTime('1992/02/01'));
+$data[] = new Person('da','Allie','','Hop', new DateTime('1989/01/02'));
+$data[] = new Person('A','Alex','van den','Hopeloze', new DateTime('1980/04/02'));
+
+/* 
+  We willen een *NETJES GEFORMATTEERD* overzicht afdrukken van de records in $data 
+  
+  Vereisten aan het overzicht:
+     - het overzicht is gesorteerd op Achternaam, Voornaam, Voorletters
+	 - Er dienen kolomkoppen te worden afgedrukt ( en een scheidingsregel hieronder )
+	 - de kolommen zijn voldoende groot, en alle data is rechts uitgelijnd
+	 	- Initialen 	=> Kolombreedte 12 tekens, elke letter gevolgd door een . en in hoofdletters
+	 	- Voornaam  	=> Kolombreedte 22 tekens
+		- Tussenvoegsel => Kolombreedte 10 tekens
+		- Achternaam	=> Kolombreedte 17 tekens
+		- Geboortedatum => Kolombreedte 12 tekens, formaat dd-mm-yyyy
+	- de kolommen hebben een spatie aan het begin en het eind van de kolom (deze zijn meegerekend in de kolombreedte)
+	
+
+hint: 
+	- (s)printf
+	- strtoupper
+	- join / split
+
+
+Success!
+
+*/
+
+
+?>
