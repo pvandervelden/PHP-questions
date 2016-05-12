6c20c93219c0be4b4dc8b7d5b461dd9336d936e9
Christiaan Nieuwlaat <krizzje@Gmail.com>
2016-05-12 11:37:54 +0200

    Create Chess.php

---
+++ Chess.php
@@ -0,0 +1,24 @@
+<?php
+
+/* 
+How about a nice game of chess:
+
+Given:
+- a chessboard has N by N possible places where N >= 8
+- a Queen can move an arbitrary amount of steps in all directions, until stopped by the chessboard boundary or another queen
+- a Knight can move in all directions in an L shape fashion ( 2up/down 1left/right or 2left/right 1 up/down)
+
+Assignment 1/2:
+- Write a PHP program which outputs all possible placements of exactly N Queens on the chessboard in 
+  such a way they don't "check" each other. BEWARE! If a placement _only differs from another by 
+  rotation or reflection of the board_, it isn't considered a different possibility
+
+- Write a PHP similar php algorithm which does the same for the maximum possible amount of Knight pieces, only 
+  this time it also needs te determine on it's own how many Knight pieces can be ultimately placed on 
+  the NxN sized board, without "checking" each other
+
+  
+Best of luck!
+  
+*/
+  
