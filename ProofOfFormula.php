7fc980050ddebdd5d58ff5394beba283ac2c03bb
Patrick van der Velden <p.vander.velden@anva.nl>
2016-05-26 09:35:50 +0200

    Linear Fibonacci-like function.

--- ProofOfFormula.php
+++ ProofOfFormula.php
@@ -4,7 +4,7 @@
 Beschouw de volgende functie:
 
 f(x) = f(x-1) + f(x-2)
-waarbij f(0) = 1 en f(1) = 1 voor alle x >= 0
+waarbij f(0) = 0 en f(1) = 1 voor alle x >= 0
 
 schrijf hiervoor een functie welke _het bewijs laat zien_ op het scherm dat f(10) = 55
 
@@ -28,4 +28,31 @@ hint:
 Succes!
 */
 
+/*
+ * =========
+ * Oplossing
+ * =========
+ *
+ * Idee:
+ * - Fibonacci-achtige reeks lineair berekenen met tail recursion:
+ *   f(n) = f(n-1) + f(n-2)
+ *   daarin waarden f(n-1) en f(n-2) meenemen als parameters:
+ *   f(n, f(n-1), f(n-2)) = f(n-1, f(n-1) + f(n-2), f(n-1))
+ *   vereenvoudigen leidt tot:
+ *   f(n, x, y) = f(n-1, x+y, x)
+ */
 
+/**
+ * @param int $n
+ * @param int $x
+ * @param int $y
+ * @return int
+ */
+function f($n, $x = 1, $y = 0)
+{
+    echo "fibonacci($n,$x,$y) = " . PHP_EOL;
+    if ($n == 1) return $x;
+    return f($n - 1, $x + $y , $x);
+}
+
+echo f(10) . PHP_EOL;
